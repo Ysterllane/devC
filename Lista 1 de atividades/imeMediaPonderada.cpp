900c8431c317e8a85c0f2943851cb5d6200480bd
Ysterllane <131874791+Ysterllane@users.noreply.github.com>
2023-08-09 19:10:30 -0300

    Add files via upload

---
+++ Lista 1 de atividades/imeMediaPonderada.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+
+    float notaDeMatematica,pesoDeMatematica, notaDeFisica, pesoDeFisica, notaDeQuimica, pesoDeQuimica, mediaPonderada;
+   
+    printf("Ei, agora vc eh um estudante que fez um concurso do IME, e agora vc vai me dizer sua nota de Matematica;\n\n");
+    scanf("%f", &notaDeMatematica);
+    
+    printf("\nQual o peso pra essa nota?\n\n");
+    scanf("%f", &pesoDeMatematica);
+    
+    printf("\nCerto, agora me diz sua nota de Fisica:\n\n");
+    scanf("%f", &notaDeFisica);
+    
+    printf("\nQual o peso pra essa nota?\n\n");
+    scanf("%f", &pesoDeFisica);
+    
+    printf("\nAgora so tah faltando sua nota de Quimica, qual foi?\n\n");
+    scanf("%f", &notaDeQuimica);
+    
+    printf("\nQual o peso pra essa nota?\n\n");
+    scanf("%f", &pesoDeQuimica);
+    
+    mediaPonderada = (notaDeMatematica*pesoDeMatematica + notaDeFisica*pesoDeFisica + notaDeQuimica*pesoDeQuimica)/(pesoDeMatematica+pesoDeQuimica+pesoDeFisica);
+    printf("\nAgora eu vou te falar o valor da sua nota por meio de um calculo chamado media ponderada:%1.f\n\n",mediaPonderada);
+    
+    return 0;
+}
---
+++ Lista 1 de atividades/lanchonete - Copia.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+
+int 01,02,03,04,05;
+
+printf("Ola Pessoo\nSeja bem vinda a nossa humilde loj ina:), ")
+
+
+
+
+}
