d13a384a87c635f83fe49bd6befe04f9a0445ba3
Ysterllane <131874791+Ysterllane@users.noreply.github.com>
2023-08-11 06:35:47 -0300

    Add files via upload

---
+++ comparaçao.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (){
+	
+	int numero = 3;
+	
+	//comparação
+	if(numero >= 7){
+		printf("aprovado");
+	}
+	
+	//se a comparação for falsa
+	else if((numero < 7)&&(numero>=4)){ // || ou uma ou outra
+		printf("final");
+	}
+	
+	//se a anterior for falsa tbm
+	else{
+		printf("reprovado");
+	}
+	
+	return 0;
+}
